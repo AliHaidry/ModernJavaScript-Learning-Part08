19f228dbf7e85f3419f5e2297b056f3449fda91f
AliHaidry <alihaidry11@gmail.com>
2021-03-08 15:28:39 -0500

    code update

--- Patterns/app.js
+++ Patterns/app.js
@@ -1,1 +1,17 @@
-alert("Hello");
+/** What is a pattern? */
+/** 
+ * A re-usable solution that can be applied to occuring problems in software design (JavaScript Applications)
+ * Can also be thought of as a programming templates
+ * Situations vary significantly
+*/
+
+/** Patterns we will look at */
+/** 
+ * Module
+ * Revealing Module Pattern
+ * Singleton
+ * Factory
+ * Observer
+ * Mediator
+ * State
+*/
